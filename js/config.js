cc5bdef69a93a417c8cf5f12638329db501a6490
meltry <meltry035@gmail.com>
2022-06-25 10:43:33 -0400

    cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_ToA4Yb8Jt', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3lunpfo3pnpt221i5uajn03ao0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
